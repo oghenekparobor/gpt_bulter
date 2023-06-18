faf50349d5d7d12fcdc78bd9ba338db44f363779
Oghenekparobor Eminokanju <robor.eminokanju@gmail.com>
2023-06-18 12:58:22 +0100

    fix: removed used imports

--- lib/presentation/views/home.dart
+++ lib/presentation/views/home.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:my_buttler/core/constant/asset.dart';
 import 'package:my_buttler/core/extensions/context.dart';
-import 'package:my_buttler/presentation/notifier/bulter.dart';
-import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
